b0b1ec3c2cb79806978bb508ed3235893d1ae15c
sudosubin <sudosubin@gmail.com>
2021-06-20 17:02:01 +0900

    Add live message update with rich text

--- src/command.py
+++ src/command.py
@@ -1,8 +1,6 @@
 import sys
 from typing import Sequence
 
-import rich
-
 from package import Package
 from package.load import load_packages_removal
 from package.status import PackageStatus
@@ -22,16 +20,16 @@ class Command(object):
     async def init(cls):
         """Common jobs for check, install packages"""
 
-        message.print_package_collecting()
+        with message.print_package_collecting():
+            # Instantiate packages
+            cls.packages = [package() for package in await Package.collection()]
 
-        # Instantiate packages
-        cls.packages = [package() for package in await Package.collection()]
+            # Package statuses
+            package_statuses = [await package.status() for package in cls.packages]
 
-        # Package statuses
-        package_statuses = [await package.status() for package in cls.packages]
-        install_count = len(tuple(filter(lambda x: x is PackageStatus.INSTALL, package_statuses)))
-        update_count = len(tuple(filter(lambda x: x is PackageStatus.UPDATE, package_statuses)))
-        removal_count = len(await load_packages_removal())
+            install_count = len(tuple(filter(lambda x: x is PackageStatus.INSTALL, package_statuses)))
+            update_count = len(tuple(filter(lambda x: x is PackageStatus.UPDATE, package_statuses)))
+            removal_count = len(await load_packages_removal())
 
         message.print_package_operations(install_count, update_count, removal_count)
 
--- src/prompt/message.py
+++ src/prompt/message.py
@@ -1,8 +1,32 @@
+import asyncio
+import contextlib
+import datetime
+
 import rich
+from rich.live import Live
 
 
+@contextlib.contextmanager
 def print_package_collecting():
-    rich.print('[blue]Collecting packages\n')
+    start_at = datetime.datetime.now()
+
+    def _get_message():
+        seconds = (datetime.datetime.now() - start_at).total_seconds()
+        duration = '{:.1f}'.format(round(seconds, 2))
+        return f'[blue]Collecting packages...[/blue] [not bold black]({duration}s)[/not bold black]'
+
+    with Live(_get_message()) as live:
+
+        async def _update_message():
+            while True:
+                live.update(_get_message(), refresh=True)
+                await asyncio.sleep(0.1)
+
+        asyncio.create_task(_update_message())
+        yield
+
+    # Empty line after all output
+    rich.print()
 
 
 def print_package_operations(install_count: int, update_count: int, removal_count: int):
