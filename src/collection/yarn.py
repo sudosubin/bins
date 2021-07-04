0d142e46e766956f1a4f33c0291a845a94808b6d
sudosubin <sudosubin@gmail.com>
2021-07-04 23:03:08 +0900

    Add yarn package

---
+++ src/collection/yarn.py
@@ -0,0 +1,13 @@
+from package import Package
+from package.source import PackageSource
+
+
+class Yarn(Package):
+    name = 'yarn'
+    description = 'Fast, reliable, and secure dependency management'
+
+    repo = 'yarnpkg/yarn'
+    source = PackageSource.GITHUB_RELEASE
+
+    asset_pattern = r'.*\.tar\.gz$'
+    bin_pattern = './**/bin/yarn'
