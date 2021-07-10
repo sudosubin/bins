79a2fa6d539de127c26f04f9fabfc66ab7c5b473
sudosubin <sudosubin@gmail.com>
2021-07-10 21:23:10 +0900

    Refact github tag, make available to download from github tag

--- src/source/github_tag.py
+++ src/source/github_tag.py
@@ -1,4 +1,4 @@
-from typing import Dict, List
+from typing import Dict, List, Optional
 
 from source.base import BasePackageSource
 from utils.configs import GITHUB_TOKEN
@@ -7,6 +7,8 @@ from utils.request import request
 
 
 class GitHubTagPackageSource(BasePackageSource):
+    _planned_version_raw: Optional[Dict] = None
+
     @staticmethod
     def _get_headers() -> Dict[str, str]:
         if GITHUB_TOKEN:
@@ -20,28 +22,36 @@ class GitHubTagPackageSource(BasePackageSource):
         version = ref.replace('refs/tags/', '')
         return semantic_release(version)
 
-    async def planned_version(self) -> str:
-        if self._planned_version is not None:
-            return self._planned_version
+    async def planned_version_raw(self) -> Dict:
+        if self._planned_version_raw is not None:
+            return self._planned_version_raw
 
         endpoint = f'https://api.github.com/repos/{self.package.repo}/git/refs/tags'
-        data = await request.get(endpoint, headers=self._get_headers())
+        data: List[Dict] = await request.get(endpoint, headers=self._get_headers())
 
         # Search for latest
         if self.package.version is None:
-            self._planned_version = self._get_tag_version(data[-1])
-            return self._planned_version
+            self._planned_version_raw = data[-1]
+            return self._planned_version_raw
 
-        tag_versions: List[str] = [
-            self._get_tag_version(tag) for tag in data if self.package.version == self._get_tag_version(tag)
-        ]
+        tag_versions: List[Dict] = [tag for tag in data if self.package.version == self._get_tag_version(tag)]
 
         if len(tag_versions) != 1:
             raise ValueError(f'Correct tag for {self.package.name} ({self.package.version}) was not found! '
                              f'({len(tag_versions)} found)')
 
-        self._planned_version = tag_versions[0]
+        self._planned_version_raw = tag_versions[0]
+        return self._planned_version_raw
+
+    async def planned_version(self) -> str:
+        if self._planned_version is not None:
+            return self._planned_version
+
+        planned_version_raw = await self.planned_version_raw()
+        self._planned_version = self._get_tag_version(planned_version_raw)
         return self._planned_version
 
     async def download_url(self) -> str:
-        raise ValueError('GitHub tag does not have any url to download.')
+        planned_version_raw = await self.planned_version_raw()
+        ref_tag_version = planned_version_raw.get('ref', '')
+        return f'https://github.com/{self.package.repo}/archive/{ref_tag_version}.tar.gz'
