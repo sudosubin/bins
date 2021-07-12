9ab9a068ee74dc4b566974022dbe8f9db1e4fa6a
sudosubin <sudosubin@gmail.com>
2021-07-12 23:02:51 +0900

    Apply fira mono font

--- src/collection/fonts/firamono.py
+++ src/collection/fonts/firamono.py
@@ -1,10 +1,5 @@
-import os
-
-from aiopath import AsyncPath
-
 from package import Package
 from package.source import PackageSource
-from utils.glob import create_symlink_base
 
 
 class FiraMono(Package):
@@ -14,19 +9,13 @@ class FiraMono(Package):
     repo = 'ryanoasis/nerd-fonts'
     source = PackageSource.GITHUB_RELEASE
 
+    link_pattern = {
+        './Fira Mono Bold Nerd Font Complete Mono.otf':
+        '~/.local/share/fonts/FiraMono/Fira Mono Bold Nerd Font Complete Mono.otf',
+        './Fira Mono Medium Nerd Font Complete Mono.otf':
+        '~/.local/share/fonts/FiraMono/Fira Mono Medium Nerd Font Complete Mono.otf',
+        './Fira Mono Regular Nerd Font Complete Mono.otf':
+        '~/.local/share/fonts/FiraMono/Fira Mono Regular Nerd Font Complete Mono.otf'
+    }
+
     asset_pattern = r'FiraMono\.zip'
-
-    async def postinstall(self):
-        allow_list = [
-            'Fira Mono Bold Nerd Font Complete Mono.otf',
-            'Fira Mono Medium Nerd Font Complete Mono.otf',
-            'Fira Mono Regular Nerd Font Complete Mono.otf',
-        ]
-
-        home_font_dir = os.path.join(os.path.expanduser('~'), '.local/share/fonts/FiraMono')
-        await AsyncPath(home_font_dir).mkdir(parents=True, exist_ok=True)
-
-        for file_name in allow_list:
-            file_dir = os.path.join(self.package_out_dir, file_name)
-            home_file_dir = os.path.join(home_font_dir, file_name)
-            await create_symlink_base(target=file_dir, dest=home_file_dir)
