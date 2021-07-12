9496bce4d70e5dcb4d915c74c2d24a0816a9919e
sudosubin <sudosubin@gmail.com>
2021-07-12 23:05:07 +0900

    Apply pretendard font

--- src/collection/fonts/pretendard.py
+++ src/collection/fonts/pretendard.py
@@ -1,10 +1,5 @@
-import os
-
-from aiopath import AsyncPath
-
 from package import Package
 from package.source import PackageSource
-from utils.glob import create_symlink_base
 
 
 class Pretendard(Package):
@@ -13,12 +8,6 @@ class Pretendard(Package):
 
     repo = 'orioncactus/pretendard'
     source = PackageSource.GITHUB_RELEASE
-
     asset_pattern = r'Pretendard-.*\.zip'
 
-    async def postinstall(self):
-        bin_font_dir = os.path.join(self.package_out_dir, 'public', 'static')
-        home_font_dir = AsyncPath(os.path.expanduser('~'), '.local/share/fonts/Pretendard')
-        await home_font_dir.parent.mkdir(parents=True, exist_ok=True)
-
-        await create_symlink_base(target=bin_font_dir, dest=home_font_dir)
+    link_pattern = {'./public/static': '~/.local/share/fonts/Pretendard'}
